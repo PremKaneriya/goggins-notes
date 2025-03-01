c95cc759876e1434c0d72f0bcf13d470d286182e
Prem Kaneriya <premkaneriyappp@gmail.com>
2025-03-01 20:39:23 +0530

    fix mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "goggins.vercel.app",
-        pathname: "/avatars/**", // Ensure it matches the correct image path
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/avatars/**",
-      },
-    ],
+    domains: ['goggins.vercel.app'],
   },
 };
 
